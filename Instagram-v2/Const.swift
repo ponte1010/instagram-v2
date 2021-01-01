356f83f22aa3e0da188ae1ebf1fb611373d78b66
ponte1010 <t.miwa.528@nitech.jp>
2021-01-02 00:53:37 +0900

    [add]写真投稿画面の作成

---
+++ Instagram-v2/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram-v2
+//
+//  Created by TECH_ACADEMY on 2021/01/02.
+//  Copyright © 2021 tetsushi.miwa. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
