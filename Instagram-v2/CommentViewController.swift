7a09fd8074bff57c039702422c0de964af413094
ponte1010 <t.miwa.528@nitech.jp>
2021-01-05 16:41:05 +0900

    [add]コメント追加画面の作成

---
+++ Instagram-v2/CommentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CommentViewController.swift
+//  Instagram-v2
+//
+//  Created by TECH_ACADEMY on 2021/01/05.
+//  Copyright © 2021 tetsushi.miwa. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
